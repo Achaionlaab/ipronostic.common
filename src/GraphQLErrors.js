b9dc77d2750f52c1931c1a2ad0736c9390d6c0dd
yair <yairopro@gmail.com>
2020-07-12 07:39:49 +0300

    GraphQLErrors.refreshToken

---
+++ src/GraphQLErrors.js
@@ -0,0 +1,10 @@
+const GraphQLErrors = {
+	refreshToken: {
+		extensions: {
+			code: "REFRESH TOKEN",
+			message: "Refresh your id token, claims have been updated.",
+		}
+	},
+};
+
+export default GraphQLErrors;
